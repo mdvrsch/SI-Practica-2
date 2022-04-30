084c14cd1ee387677a80b19b4baadcb5319229b7
[mdvrsch] <[dm.medovarschi.2020@alumnos.urjc.es]>
2022-04-30 21:04:10 +0200

    modificacion ejercicio 3 + ejercicio 4 + htmls

--- main.py
+++ main.py
@@ -6,34 +6,40 @@ import json
 import plotly.graph_objects as go
 import sqlite3
 import pandas as pd
+import requests
 
-#hola
 app = Flask(__name__)
+
 
 def dataframe_contra(con):
     df_contra = pd.read_sql_query("SELECT * FROM contrasenaTable GROUP BY nombre", con)
     return df_contra
 
+
 def dataframe_users(con):
     df_users = pd.read_sql_query(
-        "SELECT nombre, email_phishing, email_cliclados FROM usuariosTable GROUP BY nombre",con)
+        "SELECT nombre, email_phishing, email_cliclados FROM usuariosTable GROUP BY nombre", con)
     return df_users
 
+
 def dataframe_desactualizadas(con):
-    df_desactualizadas = pd.read_sql_query("SELECT nombre, cookies, aviso, proteccion FROM legalTable GROUP BY nombre",con)
+    df_desactualizadas = pd.read_sql_query("SELECT nombre, cookies, aviso, proteccion FROM legalTable GROUP BY nombre",
+                                           con)
     return df_desactualizadas
+
 
 def dataframe_emails(con):
     df_emails = pd.read_sql_query(
-        "SELECT * FROM usuariosTable GROUP BY nombre",con)
+        "SELECT * FROM usuariosTable GROUP BY nombre", con)
     return df_emails
 
 
 @app.route('/index')
 def index():
-   return render_template('index.html')
+    return render_template('index.html')
 
 
+# Ejercicio 2
 @app.route('/ejercicio2/usuarios/criticos', methods=['GET', 'POST'])
 def ejercicio2_usuariosCriticos():
     con = sqlite3.connect('database.db')
@@ -50,13 +56,14 @@ def ejercicio2_usuariosCriticos():
     df_contra["vulnerable"] = df_contra["vulnerable"].astype(int)
     df_critico = df_contra.merge(df_users, on="nombre")
     df_contra_debil = df_critico[df_critico["vulnerable"] == 1]
-    df_contra_debil["probabilidad"] = (df_contra_debil["email_cliclados"].astype(float) / df_contra_debil["email_phishing"].astype(float)) * 100
+    df_contra_debil["probabilidad"] = (df_contra_debil["email_cliclados"].astype(float) / df_contra_debil[
+        "email_phishing"].astype(float)) * 100
     df_contra_debil = df_contra_debil.sort_values("probabilidad", ascending=False)
     df_contra_debil = df_contra_debil.head(n=x)
     df_contra_debil = df_contra_debil.drop(["vulnerable"], axis=1)
 
     fig = go.Figure(
-        data=[go.Bar(x=df_contra_debil["nombre"],y=df_contra_debil["probabilidad"])],
+        data=[go.Bar(x=df_contra_debil["nombre"], y=df_contra_debil["probabilidad"])],
         layout_title_text="Gráfica resultante"
     )
 
@@ -96,16 +103,24 @@ def ejercicio2_websVulnerables():
     return render_template('ejercicio2Web.html', graphJSON_webs=graphJSON_webs)
 
 
-@app.route('/ejercicio3/info/menos50')
+# Ejercicio3
+@app.route('/ejercicio3/info/menos50', methods=['GET', 'POST'])
 def ejercicio3_menos50():
-    con = sqlite3.connect('D:\sitema\javi\GitHub\SI-Practica-2\database.db')
+    con = sqlite3.connect('database.db')
     df_emails = dataframe_emails(con)
 
-    df_emails["email_cliclados"] = df_emails["email_cliclados"].astype(float)
-    df_emails["email_phishing"]=df_emails["email_phishing"].astype(float)
-    df_emails["probabilidad"]=(( df_emails["email_cliclados"]/df_emails["email_phishing"])*100).astype(float)
-    df_emails_menos = df_emails[df_emails["probabilidad"] < 50]
+    x = 0
+    if request.method == 'POST':
+        # Then get the data from the form
+        tag = request.form['tag']
+        x = int(tag)
 
+    df_emails["email_cliclados"] = df_emails["email_cliclados"].astype(float)
+    df_emails["email_phishing"] = df_emails["email_phishing"].astype(float)
+    df_emails["probabilidad"] = ((df_emails["email_cliclados"] / df_emails["email_phishing"]) * 100).astype(float)
+    df_emails_menos = df_emails[df_emails["probabilidad"] < 50]
+    df_emails_menos = df_emails_menos.sort_values("probabilidad", ascending=False)
+    df_emails_menos = df_emails_menos.head(n=x)
 
     print(df_emails_menos)
 
@@ -120,15 +135,23 @@ def ejercicio3_menos50():
     return render_template('ejercicio3menos50.html', graphJSON_menos=graphJSON_menos)
 
 
-@app.route('/ejercicio3/info/mas50')
+@app.route('/ejercicio3/info/mas50', methods=['GET', 'POST'])
 def ejercicio3_mas50():
-    con = sqlite3.connect('D:\sitema\javi\GitHub\SI-Practica-2\database.db')
+    con = sqlite3.connect('database.db')
     df_emails = dataframe_emails(con)
 
+    x = 0
+    if request.method == 'POST':
+        # Then get the data from the form
+        tag = request.form['tag']
+        x = int(tag)
+
     df_emails["email_cliclados"] = df_emails["email_cliclados"].astype(float)
-    df_emails["email_phishing"]=df_emails["email_phishing"].astype(float)
-    df_emails["probabilidad"]=(( df_emails["email_cliclados"]/df_emails["email_phishing"])*100).astype(float)
+    df_emails["email_phishing"] = df_emails["email_phishing"].astype(float)
+    df_emails["probabilidad"] = ((df_emails["email_cliclados"] / df_emails["email_phishing"]) * 100).astype(float)
     df_emails_mas = df_emails[df_emails["probabilidad"] >= 50]
+    df_emails_mas = df_emails_mas.sort_values("probabilidad", ascending=False)
+    df_emails_mas = df_emails_mas.head(n=x)
 
     fig2 = go.Figure(
         data=[go.Bar(x=df_emails_mas["nombre"], y=df_emails_mas["probabilidad"])],
@@ -141,5 +164,26 @@ def ejercicio3_mas50():
     return render_template('ejercicio3mas50.html', graphJSON_mas=graphJSON_mas)
 
 
+# Ejercicio 4
+@app.route('/ejercicio4')
+def ejercicio4_vul():
+    response = requests.get("https://cve.circl.lu/api/last")
+
+    json_info = response.text
+    df_vul = pd.DataFrame()
+    df_vul["id"] = pd.read_json(json_info)["id"]
+    df_vul["last-modified"] = pd.read_json(json_info)["last-modified"]
+
+    fig = go.Figure(
+        data=[go.Line(x=df_vul["id"].head(n=10), y=df_vul["last-modified"].head(n=10))],
+        layout_title_text="Gráfica últimas 10 vulnerabilidades en tiempo real"
+    )
+
+    import plotly
+    a = plotly.utils.PlotlyJSONEncoder
+    graphJSON_vul = json.dumps(fig, cls=a)
+    return render_template('ejercicio4.html', graphJSON_vul=graphJSON_vul)
+
+
 if __name__ == '__main__':
-   app.run(debug=True)
+    app.run(debug=True)
