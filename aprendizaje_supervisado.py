ce81e5a9dac0ed263b41f48c9806993cf7bc486d
IselaBermejo <iselabs2001@gmail.com>
2022-05-11 12:58:09 +0200

    continuacion

--- aprendizaje_supervisado.py
+++ aprendizaje_supervisado.py
@@ -1,12 +1,16 @@
 import json
+from subprocess import call
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import datasets, linear_model
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn import tree
 from sklearn.datasets import load_iris
 import graphviz
+from sklearn.tree import export_graphviz
 
 with open('users_IA_clases.json') as file:
     data_users_entrenamiento = json.load(file)
@@ -64,10 +68,23 @@ plt.scatter(np.array(x), np.array(data_y_test), color="black")
 plt.plot((m[0][0]*np.array(x))+b, np.array(x))
 plt.show()
 
+# RANDOM FOREST
+X,y = data_x_train, data_y_train
+clf = RandomForestClassifier(max_depth=2, random_state=0,n_estimators=10)
+clf = clf.fit(data_x_train, data_y_train)
+print(str(X[0]) + " " + str(y[0]))
+print(clf.predict([X[0]]))
+
+for i in range(len(clf.estimators_)):
+    estimator = clf.estimators_[i]
+    export_graphviz(estimator, out_file='RandomForest.dot', rounded=True, proportion=False,precision=2, filled=True)
+    call(['dot','-Tpng', 'RandomForest.dot','-o','RandomForest'+str(i)+'.png','-Gdpi=600'])
+
 
 # DECISION TREE
 
-#Split data
+#Predict
+X,y = data_x_train, data_y_train
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(data_x_train, data_y_train)
 
@@ -75,20 +92,32 @@ clf = clf.fit(data_x_train, data_y_train)
 text_representation = tree.export_text(clf)
 print(text_representation)
 
-# Código de internet para mostrar el grafo
-#fig = plt.figure(figsize=(25,20))
-#imagen = tree.plot_tree(clf, feature_names=str(data_x_test), class_names=str(data_y_test), filled=True)
-#fig.savefig("decistion_tree.png")
-
-# Código diapositivas
-
-#Predict
-#clf_model = tree.DecisionTreeClassifier()
-#clf_model.fit(data_x_train, data_y_train)
-
 #Print plot
-#dot_data = tree.export_graphviz(clf, out_file=None)
-#graph = graphviz.Source(dot_data)
+fig = plt.figure(figsize=(25,20))
+imagen = tree.plot_tree(clf, feature_names=str(data_x_test), class_names=str(data_y_test), filled=True)
+fig.savefig("decistion_tree.png")
+
+dot_data = tree.export_graphviz(clf, out_file=None)
+graph = graphviz.Source(dot_data)
+graph.render("decisionTree")
+dot_data = tree.export_graphviz(clf, out_file='decisionTree.dot', filled=True, rounded=True, special_characters=True)
+graph = graphviz.Source(dot_data)
+graph.render("decisionTree.gv", view=True).replace('\\','/')
+
+
+# RANDOM FOREST
+X,y = data_x_train, data_y_train
+clf = RandomForestClassifier(max_depth=2, random_state=0,n_estimators=10)
+clf = clf.fit(data_x_train, data_y_train)
+print(str(X[0]) + " " + str(y[0]))
+print(clf.predict([X[0]]))
+
+for i in range(len(clf.estimators_)):
+    estimator = clf.estimators_[i]
+    export_graphviz(estimator, out_file='RandomForest.dot', rounded=True, proportion=False,precision=2, filled=True)
+    call(['dot','-Tpng', 'RandomForest.dot','-o','RandomForest'+str(i)+'.png','-Gdpi=600'])
+
+
 
 
 
